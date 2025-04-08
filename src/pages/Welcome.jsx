35b5efa73f1cea0f82459b9ce3ce0b896ebe26a7
davidwanlongwu <davidwu.web@gmail.com>
2025-04-08 11:52:57 -0600

    test new link

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,25 +1,27 @@
-import { Link } from 'react-router-dom';
-import logo from '@/assets/images/logo.png';
+import { Link } from "react-router-dom";
+import logo from "@/assets/images/logo.png";
 
 function Welcome() {
   return (
     <div className="flex flex-col items-center justify-between h-screen bg-white">
+      <div className="flex flex-col items-center justify-between">
+        <div className="mt-40">
+          <img src={logo} alt="Logo" className="w-44 h-auto" />
+        </div>
 
-      <div className='flex flex-col items-center justify-between'>
-      <div className="mt-40">
-        <img src={logo} alt="Logo" className="w-44 h-auto" />
-      </div>
-
-      <div className="text-5xl font-extrabold text-green-100 text-center skew-x-[-10deg] drop-shadow-[3px_3px_10px_rgba(0,0,0,1)]">
-        <p>Make the</p>
-        <p>first move</p>
-      </div>
+        <div className="text-5xl font-extrabold text-green-100 text-center skew-x-[-10deg] drop-shadow-[3px_3px_10px_rgba(0,0,0,1)]">
+          <p>Make the</p>
+          <p>first move</p>
+        </div>
       </div>
 
       <div className="flex flex-col items-center gap-10 mb-10">
         <div className="flex flex-col gap-3">
-          <button className="w-80 h-10 rounded-lg border border-[#5A3E9C]  text-[#5A3E9C] text-lg hover:bg-[#5A3E9C]">
-            Sign In
+          <button
+            className="w-80 h-10 rounded-lg border border-[#5A3E9C]  text-[#5A3E9C] text-lg hover:bg-[#5A3E9C]"
+            href="https://www.figma.com/proto/4wYsRjUyMtqiQefOZjacCq/Mockups---Design-Studio-1?page-id=988%3A1781&node-id=1482-813&viewport=558%2C-870%2C0.21&t=GsHxB01ojY8G79af-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=1070%3A3756&show-proto-sidebar=1"
+          >
+            Link to our design
           </button>
           <button className="w-80 h-10 rounded-lg border border-[#5A3E9C] text-[#5A3E9C] text-lg hover:bg-[#5A3E9C]">
             Sign Up
@@ -33,7 +35,8 @@ function Welcome() {
         </div>
         <div className="px-8 text-center text-sm text-gray-700">
           <p>
-            By signing up, you agree to our Terms. See how we use your data in our Privacy Policy. We never post to Facebook.
+            By signing up, you agree to our Terms. See how we use your data in
+            our Privacy Policy. We never post to Facebook.
           </p>
         </div>
       </div>
